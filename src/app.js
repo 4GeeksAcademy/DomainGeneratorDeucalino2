d3323fb611c76c9c5dcabc2d1b3288f25c1a26be
Deucalino Marildo Muvuma <muvumafernando@gmail.com>
2023-07-14 15:55:29 +0000

    syntax troubles solved

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let article = ["The", "A", "My"];
+let adjective = ["Quiet", "Happy", "Crappy"];
+let name = ["Man", "Cat", "Town", "Dude"];
+let extension = [".com", ".net", ".cat"];
+let domains = [];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+for (let i = 0; i < article.length; i++) {
+  for (let j = 0; j < adjective.length; j++) {
+    for (let k = 0; k < name.length; k++) {
+      for (let l = 0; l < extension.length; l++) {
+        let domainNameGenerator =
+          article[i] + adjective[j] + name[k] + extension[l];
+        console.log(domainNameGenerator);
+        domains.push(domainNameGenerator);
+      }
+    }
+  }
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("domainNameGenerator").innerHTML = domains;
+  console.log(domains);
 };
